1b31e30e7c1ba6cbedfd2ab00514f848eabfd528
sugi-01096 <nodoguro04@gmail.com>
2023-05-01 23:32:56 +0900

    諸悪の根源撃退

--- index-html/main.py
+++ index-html/main.py
@@ -13,7 +13,7 @@ power = st.sidebar.slider("威力", 0, 300, 150)
 move_type = st.sidebar.selectbox("技タイプ", ["ノーマル", "ほのお", "みず", "でんき", "くさ", "こおり", "かくとう", "どく", "じめん", "ひこう", "エスパー", "むし", "いわ", "ゴースト", "ドラゴン", "あく", "はがね", "フェアリー"])
 
 # ダメージ計算式を使ってダメージを計算する
-damage = int((((2 * level / 5 + 2) * power * attack_stat / defense_stat) / 50 + 2) * (1.5 if attack_type == move_type else 1) / 100)
+damage = int((((2 * level / 5 + 2) * power * attack_stat / defense_stat) / 50 + 2) * (1.5 if attack_type == move_type else 1) )
 
 # 計算結果を出力する
 st.header("ダメージ計算結果")
