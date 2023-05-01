086f96b3872f2fb0837441eb56525bc78ea77180
sugi-01096 <nodoguro04@gmail.com>
2023-05-02 01:00:16 +0900

    req.txt追加

--- index-html/main.py
+++ index-html/main.py
@@ -12,7 +12,7 @@ st.sidebar.header("技の情報")
 power = st.sidebar.slider("威力", 0, 300, 150)
 move_type = st.sidebar.selectbox("技タイプ", ["ノーマル", "ほのお", "みず", "でんき", "くさ", "こおり", "かくとう", "どく", "じめん", "ひこう", "エスパー", "むし", "いわ", "ゴースト", "ドラゴン", "あく", "はがね", "フェアリー"])
 
-# ダメージ計算式を使ってダメージを計算する
+# ダメージ計算式を使ってダメージをだす
 damage = int((((2 * level / 5 + 2) * power * attack_stat / defense_stat) / 50 + 2) * (1.5 if attack_type == move_type else 1) )
 
 # 計算結果を出力する
