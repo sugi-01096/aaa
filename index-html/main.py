72a53b6730e7cbf151ded86a7fe9ea54c187753d
sugi-01096 <nodoguro04@gmail.com>
2023-05-01 22:58:01 +0900

    コピペ

--- index-html/main.py
+++ index-html/main.py
@@ -1,3 +1,21 @@
 import streamlit as st
+st.title('ダメージ計算')
+# ポケモンの情報を入力するフォームを作成する
+st.sidebar.header("ポケモンの情報")
+level = st.sidebar.slider("レベル", 1, 100, 50)
+attack_type = st.sidebar.selectbox("タイプ", ["ノーマル", "ほのお", "みず", "でんき", "くさ", "こおり", "かくとう", "どく", "じめん", "ひこう", "エスパー", "むし", "いわ", "ゴースト", "ドラゴン", "あく", "はがね", "フェアリー"])
+attack_stat = st.sidebar.slider("攻撃力", 0, 1000, 500)
+defense_stat = st.sidebar.slider("防御力", 0, 1000, 500)
 
-st.title('nodo') 
+# 技の情報を入力するフォームを作成する
+st.sidebar.header("技の情報")
+power = st.sidebar.slider("威力", 0, 200, 100)
+accuracy = st.sidebar.slider("命中率", 0, 100, 100)
+move_type = st.sidebar.selectbox("タイプ相性", ["ノーマル", "ほのお", "みず", "でんき", "くさ", "こおり", "かくとう", "どく", "じめん", "ひこう", "エスパー", "むし", "いわ", "ゴースト", "ドラゴン", "あく", "はがね", "フェアリー"])
+
+# ダメージ計算式を使ってダメージを計算する
+damage = int((((2 * level / 5 + 2) * power * attack_stat / defense_stat) / 50 + 2) * (1.5 if attack_type == move_type else 1) * accuracy / 100)
+
+# 計算結果を出力する
+st.header("ダメージ計算結果")
+st.write("ダメージ: ", damage)
